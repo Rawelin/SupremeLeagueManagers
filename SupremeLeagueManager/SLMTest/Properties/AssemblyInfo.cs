04f900167a7dd971f97f1366fa640f9fa329fb23
piotrpa <piotrpa@komfort.pl>
2020-03-28 12:34:11 +0100

    Unity Test

---
+++ SupremeLeagueManager/SLMTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SLMTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SLMTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1e7d797c-211e-4723-a16d-d98e5d5cb244")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SupremeLeagueManager/SupremeLeagueManager/Models/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SupremeLeagueManager
+{
+    public class Test
+    {
+        private string Name;
+        private double Number;
+
+        public Test() { }
+      
+        public string GetName ()
+        {
+            return Name = "Supreme League Manager";
+        }
+
+        public double Sum(double p1, double p2)
+        {
+            Number = p1 + p2;
+
+            return Number;
+        }
+
+    }
+}
