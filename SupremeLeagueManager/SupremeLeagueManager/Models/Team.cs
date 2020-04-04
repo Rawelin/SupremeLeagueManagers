3cc7252b71494a2861063de6f0b805c2e6c86ded
piotrpa <piotrpa@komfort.pl>
2020-04-04 16:18:31 +0200

    part1 class team controler Team Test

---
+++ SupremeLeagueManager/SupremeLeagueManager/Models/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SupremeLeagueManager.Models
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public List<Player> Players { get; set; }
+
+        private double teamPower;
+        
+        public Team() { }
+
+        public double GetTeamPower()
+        {
+            return teamPower;
+        }
+
+        public string GetFullName()
+        {
+            return Name + " " + City;
+        }
+    }
+}
