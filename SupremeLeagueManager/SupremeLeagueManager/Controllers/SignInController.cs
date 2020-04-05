d86592b82580d7c3a87d9b9d6d38aa60f32cd469
Pawel Karasinski <pawelkarasinski1981@o2.pl>
2020-04-05 11:50:08 +0200

    2020-04-05 01

---
+++ SupremeLeagueManager/SupremeLeagueManager/Controllers/SignInController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SupremeLeagueManager.Models.SignIn;
+
+namespace SupremeLeagueManager.Controllers
+{
+    public class SignInController : Controller
+    {
+        // GET: SignIn
+        public ActionResult SignIn()
+        {
+            SignInVM signInVM = new SignInVM();
+
+            return View(signInVM);
+        }
+
+        public ActionResult _SignIn()
+        {
+            UsersM users = new UsersM();
+
+            return PartialView(users);
+        }
+
+        public ActionResult _Registration()
+        {
+            UsersM users = new UsersM();
+
+            return PartialView(users);
+        }
+    }
+}
---
+++ SupremeLeagueManager/SupremeLeagueManager/Models/SignIn/UsersM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SupremeLeagueManager.Models.SignIn
+{
+    public class UsersM
+    {
+        public int IdUser { get; set; }
+        public string eMail { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime LastLoginDate { get; set; }
+        public int AccountActivated { get; set; }
+        public int Active { get; set; }
+    }
+}
