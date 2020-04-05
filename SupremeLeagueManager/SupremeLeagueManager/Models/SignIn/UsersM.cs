97a7cb534afb53f2705b581e3dcd89bcf5fd3482
Pawel Karasinski <pawelkarasinski1981@o2.pl>
2020-04-05 14:28:59 +0200

    2020-04-05 - end of day.

--- SupremeLeagueManager/SupremeLeagueManager/Models/SignIn/UsersM.cs
+++ SupremeLeagueManager/SupremeLeagueManager/Models/SignIn/UsersM.cs
@@ -3,16 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using SLMContextDB;
+
 namespace SupremeLeagueManager.Models.SignIn
 {
-    public class UsersM
+    public class UsersM : Users
     {
-        public int IdUser { get; set; }
-        public string eMail { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime LastLoginDate { get; set; }
-        public int AccountActivated { get; set; }
-        public int Active { get; set; }
+
     }
 }
