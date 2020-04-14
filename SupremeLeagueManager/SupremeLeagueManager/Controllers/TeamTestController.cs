f16fa91abedbaa4bbb27a4c4a3a4148c7fa85bbf
piotrpa <piotrpa@komfort.pl>
2020-04-14 19:10:48 +0200

    b007 partial view add

--- SupremeLeagueManager/SupremeLeagueManager/Controllers/TeamTestController.cs
+++ SupremeLeagueManager/SupremeLeagueManager/Controllers/TeamTestController.cs
@@ -15,7 +15,17 @@ namespace SupremeLeagueManager.Controllers
             return View(new CreateTeams().GetTeams());
         }
 
+        public ActionResult _TeamManagement(Provider provider)
+        {
+            return PartialView(new TeamMenagement(provider).GetTeam());
+        }
+
         public ActionResult _TeamSquad(Provider provider)
+        {
+            return PartialView(new TeamMenagement(provider).GetTeam());
+        }
+
+        public ActionResult _TeamSkills(Provider provider)
         {
             return PartialView(new TeamMenagement(provider).GetTeam());
         }
