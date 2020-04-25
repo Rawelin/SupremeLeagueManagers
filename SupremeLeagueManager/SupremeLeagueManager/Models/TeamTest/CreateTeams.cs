bcf523d7ccb58adb68f2c1772744812c0df03c91
piotrpa <piotrpa@komfort.pl>
2020-04-25 17:40:28 +0200

    b008 cleaning

--- SupremeLeagueManager/SupremeLeagueManager/Models/TeamTest/CreateTeams.cs
+++ SupremeLeagueManager/SupremeLeagueManager/Models/TeamTest/CreateTeams.cs
@@ -1,4 +1,5 @@
 ﻿using SLMContextDB;
+using SupremeLeagueManager.Models.Global;
 using SupremeLeagueManager.Models.Global_Models;
 using System;
 using System.Collections.Generic;
@@ -22,15 +23,38 @@ namespace SupremeLeagueManager.Models.TeamTest
         }
         private void SetTeams()
         {
+            //try
+            //{
+            //    TeamTestVM.Team = new Team();
+            //    TeamTestVM.Teams.AddRange(new MockTeam().GetTeams());
+            //}
+            //catch (Exception ex)
+            //{
+            //    TeamTestVM.ErrorMessage = ex.ToString();
+            //}
+
             try
             {
-                TeamTestVM.Team = new Team();
-                TeamTestVM.Teams.AddRange(new MockTeam().GetTeams());
+                using (Entities slmCtx = new Entities())
+                {
+                    TeamTestVM.Teams = slmCtx.dictTeams.Where(t => t.Active == 1)
+                                                       .Select(t => new Team
+                                                       {
+                                                           TeamId = t.IdDictTeams,
+                                                           Name = t.Name,
+                                                           City = t.City
+
+                                                       })
+                                                       .ToList();
+                                                       
+                }
             }
             catch (Exception ex)
             {
-                TeamTestVM.ErrorMessage = ex.ToString();
+                ErrorHandling.InsertError("TeamMeanagement", "CreateTeams", "SetTeams", ex);
+
             }
+
         }
     }
 }
