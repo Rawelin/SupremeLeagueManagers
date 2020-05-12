be320abb8bfd0f590599170e6a2413087a584813
piotrpa <piotrpa@komfort.pl>
2020-05-12 20:18:48 +0200

    b010 DynamicSkills

--- SupremeLeagueManager/SupremeLeagueManager/Scripts/Skills/Skills.js
+++ SupremeLeagueManager/SupremeLeagueManager/Scripts/Skills/Skills.js
@@ -1,4 +1,4 @@
-﻿// static -  no position on the field  includes
+﻿// static -  excludes position on the field 
 function PlayerSkills(player) {
   
     var averageSkill = 0;
@@ -9,7 +9,7 @@ function PlayerSkills(player) {
             player.AverageSkills = player.AverageSkills.toFixed(0);
             break;
         case 'CB':
-            player.AverageSkills = (player.Defence + player.Tackling + player.Covering + player.Heading + player.Speed + player.Acceleration) / 6;
+            player.AverageSkills = (player.Defence + player.Tackling + player.Covering + player.Heading) / 4;
             player.AverageSkills = player.AverageSkills.toFixed(0);
             break;
         case 'LB':
@@ -58,14 +58,73 @@ function PlayerSkills(player) {
 
     return averageSkill;
 }
-// static -  no position on the field  includes
+
+// dynamic - includes position on the field
+function PlayerSkillsDynamic(player, formation) {
+
+    var currentPosition = player.IndexPosition;
+    var originalPosition = player.PositionShort;
+    var dynamicAverageSkill = 0;
+
+    //console.log(player.IndexPosition + ' ' + player.Name + ' ' +player.PositionShort);
+
+    if (formation == 1) {
+        switch (currentPosition) {
+
+            case 0:
+                player.PositionShort = 'GK';
+                player.AverageDynamicSkills = PlayerSkills(player);
+                break;
+            case 1:
+                player.PositionShort = 'LB';
+                player.AverageDynamicSkills = (PlayerSkills(player) * player.LeftFootSkills * 0.01).toFixed(0);
+                break;
+            case 2:
+            case 3:
+                player.PositionShort = 'CB';
+                player.AverageDynamicSkills = PlayerSkills(player);
+                break;
+            case 4:
+                player.PositionShort = 'RB';
+                player.AverageDynamicSkills = (PlayerSkills(player) * player.RightFootSkills * 0.01).toFixed(0);
+                break;
+            case 5:
+                player.PositionShort = 'LM';
+                player.AverageDynamicSkills = (PlayerSkills(player) * player.LeftFootSkills * 0.01).toFixed(0);
+                break;
+            case 6:
+                player.PositionShort = 'DM';
+                player.AverageDynamicSkills = PlayerSkills(player);
+                break;
+            case 7:
+                player.PositionShort = 'CM';
+                player.AverageDynamicSkills = PlayerSkills(player);
+                break;
+            case 8:
+                player.PositionShort = 'RM';
+                player.AverageDynamicSkills = (PlayerSkills(player) * player.RightFootSkills * 0.01).toFixed(0);
+                break;
+            case 9:  
+            case 10:
+                player.PositionShort = 'CF';
+                player.AverageDynamicSkills = PlayerSkills(player);
+                break;
+        }
+    }
+
+    player.PositionShort = originalPosition;
+
+    return dynamicAverageSkill = player.AverageDynamicSkills;
+}
+
+// dynamic - includes position on the field
 function TeamSkills(team) {
 
     // 1. 4-4-2
     // 2. 4-3-3
     // 3. 4.5.1
     // 4. 3.5.2
-
+   
     var goalKeeper = defence =  midfield =  attack = speed = technique =  overall = 0;
 
     switch (team.Formation) {
@@ -73,7 +132,7 @@ function TeamSkills(team) {
         case 2:
         case 3:
             for (var i = 1; i < 5; i++) {
-                defence += parseInt(PlayerSkills(team.Players[i]));
+                defence += parseInt(PlayerSkillsDynamic(team.Players[i], team.Formation));
             }
             defence /= 4;
             break;
@@ -88,7 +147,7 @@ function TeamSkills(team) {
     switch (team.Formation) {
         case 1:
             for (var i = 5; i < 9; i++) {
-                midfield += parseInt(PlayerSkills(team.Players[i]));
+                midfield += parseInt(PlayerSkillsDynamic(team.Players[i], team.Formation));
             }
             midfield /= 4;
             break;
@@ -115,7 +174,7 @@ function TeamSkills(team) {
     switch (team.Formation) {
         case 1:
             for (var i = 9; i < 11; i++) {
-                attack += parseInt(PlayerSkills(team.Players[i]));
+                attack += parseInt(PlayerSkillsDynamic(team.Players[i], team.Formation));
             }
             attack /= 2;
             break;
@@ -140,7 +199,7 @@ function TeamSkills(team) {
 
     if (team.Formation) {
 
-        goalKeeper = parseInt(PlayerSkills(team.Players[0]));
+        goalKeeper = parseInt(PlayerSkillsDynamic(team.Players[0], team.Formation));
 
         for (var i = 1; i < 11; i++) {
             speed += team.Players[i].Speed;
@@ -149,8 +208,6 @@ function TeamSkills(team) {
         speed /= 10;
         technique /= 10;
     }
-
-    console.log(midfield);
 
     team.GoalKeeper = goalKeeper.toFixed(0);;
     team.Defence = defence.toFixed(0);;
