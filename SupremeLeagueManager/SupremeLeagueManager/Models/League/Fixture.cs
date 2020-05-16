887c1ab80f06a5815c618c978113b6152b0f603f
piotrpa <piotrpa@komfort.pl>
2020-05-16 11:33:30 +0200

    b010 Fixture cd

--- SupremeLeagueManager/SupremeLeagueManager/Models/League/Fixture.cs
+++ SupremeLeagueManager/SupremeLeagueManager/Models/League/Fixture.cs
@@ -12,7 +12,7 @@ namespace SupremeLeagueManager.Models.League
     {
         public List<Tuple<Team, Team>> pairs = new List<Tuple<Team, Team>>();
         public List<Tuple<Team, Team>> pairs2 = new List<Tuple<Team, Team>>();
-        public List<List<Tuple<Team, Team>>> pairFixtures = new List<List<Tuple<Team, Team>>>();
+        public List<Tuple<Team, Team>> pairFixtures = new List<Tuple<Team, Team>>();
 
         public List<Team> teamsA = new List<Team>();
         public List<Team> teamsB = new List<Team>();
@@ -36,61 +36,123 @@ namespace SupremeLeagueManager.Models.League
                     teamsB.RemoveAt(0);
                 }
 
-                pairs2.Add(pairs[0]);
-
-                Debug.Write(pairs2[0].Item1.City);
-                Debug.Write("-");
-                Debug.Write(pairs2[0].Item2.City);
-                Debug.WriteLine("");
-
-                int fixture = 1;
-
-
-                for (int i = 0; i < pairs2.Count + 1;)
+                
+                
+                for (int i = 0; i < 17; i++)
                 {
-                    for (int j = 0; j < pairs.Count; j++)
+                    pairs2.Add(pairs.First());
+                   
+                   
+                    for (int j = 0; j < pairs.Count(); j++)
                     {
-                        if (pairs2[i].Item1.TeamId != pairs[j].Item1.TeamId && pairs2[i].Item2.TeamId != pairs[j].Item2.TeamId &&
-                            pairs2[i].Item2.TeamId != pairs[j].Item1.TeamId && pairs2[i].Item1.TeamId != pairs[j].Item2.TeamId)
+                        
+                        bool exist = true;
+                        for (int k = 0; k < pairs2.Count; k++)
                         {
-                            var check = pairs.Where(f => f != pairs[j]).First();
-                            if(!(check is null))
+                            Debug.Write(pairs2[k].Item1.TeamId);
+                            Debug.Write(" ");
+                            Debug.Write(pairs2[k].Item2.TeamId);
+                            Debug.WriteLine(" F ");
+                            Debug.Write(pairs[j].Item1.TeamId);
+                            Debug.Write(" ");
+                            Debug.WriteLine(pairs[j].Item2.TeamId);
+                            Debug.WriteLine("-------");
+
+                            if (pairs2[k].Item1.TeamId != pairs[j].Item1.TeamId && pairs2[k].Item1.TeamId != pairs[j].Item2.TeamId &&
+                                pairs2[k].Item2.TeamId != pairs[j].Item1.TeamId && pairs2[k].Item2.TeamId != pairs[j].Item2.TeamId)
                             {
-                                pairs2.Add(pairs[j]);
-                                pairs.RemoveAt(j);
-                                i++;
-
-                                Debug.Write(pairs2[i].Item1.Name);
-                                Debug.Write(" ");
-                                Debug.Write(pairs2[i].Item1.City);
-                                Debug.Write(" - ");
-                                Debug.Write(pairs2[i].Item2.Name);
-                                Debug.Write(" ");
-                                Debug.Write(pairs2[i].Item2.City);
-                                Debug.WriteLine("");
-
-                                if (pairs2.Count() % 9 == 0)
-                                {
-                                    fixture++;
-                                    Debug.Write("FIXTURE ");
-                                    Debug.Write(fixture);
-                                    Debug.WriteLine("");
-                                }
-
+                                exist = false;
+                               
+                               
                             }
-                           
-                           
+                            else
+                            {
+                                exist = true;
+                            }
                         }
-                        if (fixture == 18)
+                        
+
+                        if (exist == false)
                         {
-                            break;
+                            pairs2.Add(pairs[j]);
+                            pairs.Remove(pairs[j]);
+                            exist = false;
+
+                            if (pairs2.Count() % 9 == 0)
+                            {
+                                pairFixtures.AddRange(pairs2);
+                                pairs2.Clear();
+                                pairs.Remove(pairs.First());
+                            }
                         }
                     }
                 }
+
+
+                //pairs2.Add(pairs[0]);
+
+                //Debug.Write(pairs2[0].Item1.City);
+                //Debug.Write("-");
+                //Debug.Write(pairs2[0].Item2.City);
+                //Debug.WriteLine("");
+
+                //int fixture = 1;
+
+
+                //for (int i = 0; i < pairs2.Count + 1;)
+                //{
+                //    for (int j = 0; j < pairs.Count; j++)
+                //    {
+                //        if (pairs2[i].Item1.TeamId != pairs[j].Item1.TeamId && pairs2[i].Item2.TeamId != pairs[j].Item2.TeamId &&
+                //            pairs2[i].Item2.TeamId != pairs[j].Item1.TeamId && pairs2[i].Item1.TeamId != pairs[j].Item2.TeamId)
+                //        {
+
+                //            //var exist = pairs2.Where(f => f.Item1.TeamId != pairs[j].Item1.TeamId &&
+                //            //                              f.Item2.TeamId != pairs[j].Item2.TeamId &&
+                //            //                              f.Item2.TeamId != pairs[j].Item1.TeamId &&
+                //            //                              f.Item1.TeamId != pairs[j].Item2.TeamId).First();
+
+                //            var check = pairs2.Where(f => f != pairs[j]).First();
+                //        if (!(check is null))
+                //            {
+                //                pairs2.Add(pairs[j]);
+                //                pairs.RemoveAt(j);
+                //                i++;
+
+                //                Debug.Write(pairs2[i].Item1.Name);
+                //                Debug.Write(" ");
+                //                Debug.Write(pairs2[i].Item1.City);
+                //                Debug.Write(" - ");
+                //                Debug.Write(pairs2[i].Item2.Name);
+                //                Debug.Write(" ");
+                //                Debug.Write(pairs2[i].Item2.City);
+                //                Debug.WriteLine("");
+
+                //                exist = false;
+                //                    fixture++; 
+                //                    Debug.Write(fixture);
+                //                    Debug.WriteLine("");
+                //                // add pairs2 to pairFixtures
+                //                pairFixtures.AddRange(pairs2);
+                //                // clear pairs2
+                //                pairs2.Clear(); 
+
+                //                }
+
+                //            }
+
+
+                //        }
+                //        if (fixture == 18)
+                //        {
+                //            break;
+                //        }
+                //    }
+                //}
             }
             catch (Exception ex)
             {
-
+                Debug.Write(ex.ToString());
             }
         }
 
