311f074f948c15e30d816556b5d97202790fd1b0
piotrpa <piotrpa@komfort.pl>
2020-10-28 18:47:18 +0100

    b011 MusicPlayer

---
+++ SupremeLeagueManager/SupremeLeagueManager/Scripts/GlobalFunctions/MusicPlayer.js
@@ -0,0 +1,44 @@
+﻿
+function songShuffle(player, path, quantity) {
+
+    var songs = []
+
+    for (var i = 1; i < quantity; i++) {
+        songs.push(path + i + ".mp3")
+    }
+
+    for (let i = songs.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i)
+        const temp = songs[i]
+        songs[i] = songs[j]
+        songs[j] = temp
+    }
+    musicPlayer(player, songs);
+}
+
+function musicPlayer(music_player, songs) {
+
+    var i = 0;
+
+    function next() {
+        // Check for last audio file in the playlist
+        if (i === songs.length - 1) {
+            i = 0;
+        } else {
+            i++;
+        }
+
+        // Change the audio element source
+        music_player.src = songs[i];
+    }
+
+    if (music_player === null) {
+        throw "Playlist Player does not exists ...";
+    } else {
+        // Start the player
+        music_player.src = songs[i];
+
+        // Listen for the music ended event, to play the next audio file
+        music_player.addEventListener('ended', next, false)
+    }
+}
