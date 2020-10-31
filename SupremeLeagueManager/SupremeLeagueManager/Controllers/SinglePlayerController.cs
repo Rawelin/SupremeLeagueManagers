b66978437e55a32ac3b0aa16255d477a079abef3
piotrpa <piotrpa@komfort.pl>
2020-10-31 17:34:59 +0100

    b011 SinglePlayer menu

---
+++ SupremeLeagueManager/SupremeLeagueManager/Controllers/SinglePlayerController.cs
@@ -0,0 +1,18 @@
+﻿using SupremeLeagueManager.Models.TeamTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupremeLeagueManager.Controllers
+{
+    public class SinglePlayerController : Controller
+    {
+        // GET: SinglePlayer
+        public ActionResult SinglePlayer()
+        {
+            return View(new CreateTeams().GetTeams());
+        }
+    }
+}
