fbd63ec378bba966abed7d21ff9ff4fb0fc4d435
piotrpa <piotrpa@komfort.pl>
2020-11-06 18:01:19 +0100

    b011 music

---
+++ SupremeLeagueManager/SupremeLeagueManager/Controllers/LogoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupremeLeagueManager.Controllers
+{
+    public class LogoController : Controller
+    {
+        // GET: Logo
+        public ActionResult Logo()
+        {
+            return View();
+        }
+    }
+}
